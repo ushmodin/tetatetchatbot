bb4cd5e64623ed746bb35d6662f75021d08c4a6f
Ushmodin Nikolay <ushmodin@gmail.com>
2018-09-28 09:22:08 +0700

    Refactoring

--- telegram/bot.go
+++ telegram/bot.go
@@ -1,6 +1,8 @@
 package telegram
 
 import (
+	"log"
+
 	"github.com/globalsign/mgo/bson"
 )
 
@@ -9,19 +11,38 @@ type Bot struct {
 	telegram *TelegramClient
 }
 
+type DialogStatus string
+type UserStatus string
+
+const (
+	DIALOG_STATUS_ACTIVE  DialogStatus = "A"
+	DIALOG_STATUS_DELETED DialogStatus = "D"
+	DIALOG_STATUS_REQUEST DialogStatus = "R"
+)
+
+type Dialog struct {
+	ID      bson.ObjectId `json:"_id,omitempty"`
+	UserA   bson.ObjectId
+	AcceptA bool
+	UserB   bson.ObjectId
+	AcceptB bool
+	Status  DialogStatus
+}
+
 type BotUser struct {
-	ID           int
+	ID           bson.ObjectId `json:"_id,omitempty"`
+	TelegramID   int
 	Name         string
 	LanguageCode string
-	Status       string
+	Status       UserStatus
 	Pause        bool
-	ChatId       int64
-	DialogId     bson.ObjectId
+	ChatID       int64
+	DialogID     bson.ObjectId
 }
 
 const (
-	USER_STATUS_SEARCH        = "S"
-	USER_STATUS_COMMUNICATION = "C"
+	USER_STATUS_SEARCH        UserStatus = "S"
+	USER_STATUS_COMMUNICATION UserStatus = "C"
 )
 
 func NewBot(db *Db, telegram *TelegramClient) (*Bot, error) {
@@ -29,28 +50,94 @@ func NewBot(db *Db, telegram *TelegramClient) (*Bot, error) {
 }
 
 func (bot Bot) Start(user *User, chat *Chat) error {
-	botUser, err := bot.db.FindUser(user.ID)
+	botUser, err := bot.db.FindUserByTelegramId(user.ID)
 	if bot.db.IsNotFound(err) {
-		botUser.ID = user.ID
+		log.Println("New User " + string(user.ID))
+		botUser.TelegramID = user.ID
 		botUser.Name = user.FirstName
 		botUser.LanguageCode = user.LanguageCode
-		botUser.ChatId = chat.ID
+		botUser.ChatID = chat.ID
 		botUser.Status = USER_STATUS_SEARCH
 		botUser.Pause = true
-		err = bot.db.SaveUser(botUser)
+	} else if err != nil {
+		return err
 	}
+
+	if botUser.DialogID.Valid() {
+		dialog, err := bot.db.FindDialog(botUser.DialogID)
+		if bot.db.IsNotFound(err) {
+			log.Println("Dialog not found: " + botUser.DialogID)
+			botUser.DialogID = ""
+		}
+		dialogActive, err := bot.IsDialogActive(dialog)
+		if err != nil {
+			return err
+		}
+		if !dialogActive {
+			log.Println("Dialog not active: " + botUser.DialogID)
+			botUser.DialogID = ""
+		}
+	}
+
+	err = bot.db.SaveUser(botUser)
+	log.Println("User activated " + string(user.ID))
+	return err
+}
+
+func (bot Bot) IsDialogActive(dialog Dialog) (bool, error) {
+	if dialog.Status == DIALOG_STATUS_DELETED {
+		return false, nil
+	}
+	userA, err := bot.db.FindUser(dialog.UserA)
+	if err != nil {
+		return false, err
+	}
+	if userA.DialogID != dialog.ID {
+		return false, nil
+	}
+	userB, err := bot.db.FindUser(dialog.UserB)
+	if err != nil {
+		return false, err
+	}
+	if userB.DialogID != dialog.ID {
+		return false, nil
+	}
+	return true, nil
+}
+
+func (bot Bot) Search(user *User) error {
+	botUser, err := bot.db.FindUserByTelegramId(user.ID)
+	if err != nil {
+		return err
+	}
+	if botUser.DialogID != "" {
+		dialog, err := bot.db.FindDialog(botUser.DialogID)
+		if err != nil {
+			return err
+		}
+		dialog.Status = DIALOG_STATUS_DELETED
+		err = bot.db.DeleteDialog(dialog.ID)
+		if err != nil {
+			return err
+		}
+	}
+	err = bot.db.UpdateUserStatus(botUser.ID, USER_STATUS_SEARCH)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (bot Bot) Pause(user *User) error {
+	botUser, err := bot.db.FindUserByTelegramId(user.ID)
 	if err != nil {
 		return err
 	}
 
-	return nil
-}
-
-func (bot Bot) Search() error {
-	return nil
-}
-
-func (bot Bot) Pause() error {
+	err = bot.db.UpdateUserPause(botUser.ID, !botUser.Pause)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
--- telegram/db.go
+++ telegram/db.go
@@ -33,12 +33,36 @@ func (db Db) IsNotFound(err error) bool {
 	return err == mgo.ErrNotFound
 }
 
-func (db Db) FindUser(id int) (BotUser, error) {
+func (db Db) FindUserByTelegramId(id int) (BotUser, error) {
 	var user BotUser
-	err := db.mongo.DB(db.db).C("users").Find(bson.M{"telegramID": id}).One(&user)
+	err := db.mongo.DB(db.db).C("users").Find(bson.M{"TelegramID": id}).One(&user)
+	return user, err
+}
+
+func (db Db) FindUser(id bson.ObjectId) (BotUser, error) {
+	var user BotUser
+	err := db.mongo.DB(db.db).C("users").Find(bson.M{"_id": id}).One(&user)
 	return user, err
 }
 
 func (db Db) SaveUser(user BotUser) error {
 	return db.mongo.DB(db.db).C("users").Insert(user)
+}
+
+func (db Db) FindDialog(id bson.ObjectId) (Dialog, error) {
+	var dialog Dialog
+	err := db.mongo.DB(db.db).C("dialogs").Find(bson.M{"_id": id}).One(&dialog)
+	return dialog, err
+}
+
+func (db Db) DeleteDialog(id bson.ObjectId) error {
+	return db.mongo.DB(db.db).C("dialogs").Update(bson.M{"_id": id}, bson.M{"Status": DIALOG_STATUS_DELETED})
+}
+
+func (db Db) UpdateUserStatus(id bson.ObjectId, status UserStatus) error {
+	return db.mongo.DB(db.db).C("dialogs").Update(bson.M{"_id": id}, bson.M{"Status": status})
+}
+
+func (db Db) UpdateUserPause(id bson.ObjectId, flag bool) error {
+	return db.mongo.DB(db.db).C("dialogs").Update(bson.M{"_id": id}, bson.M{"Pause": flag})
 }
