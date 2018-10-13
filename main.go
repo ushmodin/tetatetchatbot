c57b2a3be63f5976371d2023da0d2e0988f524b6
Nikolay U <ushmodin@gmail.com>
2018-10-13 23:11:18 +0700

    docker compose

--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/ushmodin/tetatetchatbot/telegram"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/ushmodin/tetatetchatbot/telegram"
 )
 
 func main() {
 	router := mux.NewRouter()
-	client, _ := telegram.NewTelegramClient("")
+	token := os.Getenv("TELEGRAM_TOKEN")
+	if token == "" {
+		log.Fatal("Telegram token not specified")
+	}
+	client, _ := telegram.NewTelegramClient(token)
 	router.HandleFunc("/t/b/tetatet/updates", client.UpdateHandler).Methods("POST")
 	router.HandleFunc("/ping", client.PingHandler).Methods("GET")
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
