8bc4e6efeecd60c456365f14ed42c92a0f71122b
Nikolay U <ushmodin@gmail.com>
2018-11-03 09:14:06 +0700

    tests

--- go.mod
+++ go.mod
@@ -2,8 +2,11 @@ module github.com/ushmodin/tetatetchatbot
 
 require (
 	github.com/boltdb/bolt v1.3.1
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/globalsign/mgo v0.0.0-20180905125535-1ca0a4f7cbcb
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.2.2
 	gopkg.in/resty.v1 v1.9.1
 )
--- telegram/bot_test.go
+++ telegram/bot_test.go
@@ -1,52 +1,48 @@
 package telegram
 
 import (
-	"fmt"
-	"log"
 	"math/rand"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 )
 
 var db *BoltDb
 var ms *BoltMessageService
 var bot *Bot
 
-func setUp() error {
+func TestBotRun(t *testing.T) {
+	suite.Run(t, new(BotSuite))
+}
+
+type BotSuite struct {
+	suite.Suite
+}
+
+func (suite *BotSuite) SetupTest() {
 	var err error
 	db, err = NewBoltDb()
 	if err != nil {
-		return err
+		suite.Fail("Can't create bot", err)
 	}
 	ms, err = NewBoltMessageService()
 	if err != nil {
-		return err
+		suite.Fail("Can't create message service", err)
 	}
 	bot, err = NewBot(db, ms)
-	return err
 }
 
-func tearDown() error {
-	defer db.Close()
-	defer ms.Close()
+func (suite *BotSuite) TearDownTest() {
+	db.Close()
+	ms.Close()
 	os.Remove("bms.db")
 	os.Remove("tetatetchatbot.db")
-	return nil
 }
 
-func TestMain(m *testing.M) {
-	err := setUp()
-	if err != nil {
-		log.Panic(err)
-	}
-	retCode := m.Run()
-	tearDown()
-	os.Exit(retCode)
-}
-
-func TestStart(t *testing.T) {
+func (suite *BotSuite) TestStart() {
 	user := User{
-		ID:           42,
+		ID:           rand.Int(),
 		FirstName:    "Ivan",
 		LastName:     "Ivanov",
 		UserName:     "IvanovIvan",
@@ -54,35 +50,17 @@ func TestStart(t *testing.T) {
 		IsBot:        false,
 	}
 	chat := Chat{
-		ID: 100500,
+		ID: rand.Int63(),
 	}
-	err := bot.Start(user, chat)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = bot.Start(user, chat)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if ms == nil {
-		log.Panic("Service is nil2")
-	}
-
+	suite.NoError(bot.Start(user, chat), "Can't  start user")
+	suite.NoError(bot.Start(user, chat), "Can't  start user")
 	messages, err := ms.Next10()
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Println(len(messages))
-	if len(messages) != 2 {
-		t.Fatal("Incorrect message count")
-	}
-	if messages[0].ChatID != 100500 {
-		t.Fatal("Incorrect response message")
-	}
+	suite.Nil(err, "Can't get messages")
+	suite.Equal(2, len(messages), "Incorrect message count")
+	suite.Equal(chat.ID, messages[0].ChatID, "Incorrect message count")
 }
 
-func TestJoinRequests(t *testing.T) {
+func (suite *BotSuite) TestJoinRequests() {
 	userA := User{
 		ID:           rand.Int(),
 		FirstName:    "Ivan",
@@ -105,31 +83,16 @@ func TestJoinRequests(t *testing.T) {
 	chatB := Chat{
 		ID: rand.Int63(),
 	}
-	if err := bot.Start(userA, chatA); err != nil {
-		t.Fatal(err)
-	}
-	if err := bot.Start(userB, chatB); err != nil {
-		t.Fatal(err)
-	}
-	if err := bot.Search(userA); err != nil {
-		t.Fatal(err)
-	}
-	if err := bot.Search(userB); err != nil {
-		t.Fatal(err)
-	}
-	ok, err := bot.JoinRequests()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !ok {
-		t.Fatal("Requests not joined")
-	}
-	messages, err := ms.Next10()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(messages) != 4 {
-		t.Fatal("Incorrect message count")
-	}
+	suite.NoError(bot.Start(userA, chatA), "Can't start user")
+	suite.NoError(bot.Start(userB, chatB), "Can't start user")
 
+	suite.NoError(bot.Search(userA), "Can't start user")
+	suite.NoError(bot.Search(userB), "Can't start user")
+
+	ok, err := bot.JoinRequests()
+	suite.NoError(err, "Join error")
+	suite.True(ok, "Requests not joined")
+	messages, err := ms.Next10()
+	suite.NoError(err)
+	suite.Equal(4, len(messages), "Incorrect message count")
 }
